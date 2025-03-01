46d2b16e954b3b11d561d72765ab352134bade01
rosconpasa <willdozawill@gmail.com>
2025-03-01 12:27:05 -0500

    a

---
+++ Practica/myScript.js
@@ -0,0 +1,10 @@
+document.getElementById("frase").innerHTML = "Hola"; //para reemplazar o editar un elemento html  
+document.getElementById("frase").innerHTML = "<h1><strong>Hola</strong></h1>"; 
+document.getElementById("frase2").innerText = "Adiós amigos"; //Para editar el texto plano
+
+// document.write("Texto"); esto borrará todos los elementos html si se ejecuta después de cargarse la página
+// window.alert("ALERTAAAA");
+console.log("4+4=")
+console.log(4+4)
+
+
